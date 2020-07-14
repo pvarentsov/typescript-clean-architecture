cb754ced85826c49433d54f781d55879e0bf0c20
pvarentsov <varentsovpavel@gmail.com>
2020-07-14 19:40:03 +0300

    Add NestPostImageRemovedEventHandler & NestDoesMediaExistQueryHandler

---
+++ src/infrastructure/handler/media/NestPostImageRemovedEventHandler.ts
@@ -0,0 +1,20 @@
+import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
+import { Inject, Injectable } from '@nestjs/common';
+import { MediaRemovedEvent } from '../../../core/common/cqers/event/events/media/MediaRemovedEvent';
+import { PostDITokens } from '../../../core/domain/post/di/PostDITokens';
+import { PostImageRemovedEventHandler } from '../../../core/domain/post/handler/PostImageRemovedEventHandler';
+
+@Injectable()
+@EventsHandler(MediaRemovedEvent)
+export class NestPostImageRemovedEventHandler implements IEventHandler {
+  
+  constructor(
+    @Inject(PostDITokens.PostImageRemovedEventHandler)
+    private readonly handleService: PostImageRemovedEventHandler
+  ) {}
+
+  public async handle(event: MediaRemovedEvent): Promise<void> {
+    return this.handleService.handle(event);
+  }
+  
+}
---
+++ src/infrastructure/handler/post/NestDoesMediaExistQueryHandler.ts
@@ -0,0 +1,21 @@
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { DoesMediaExistQuery } from '../../../core/common/cqers/query/queries/media/DoesMediaExistQuery';
+import { DoesMediaExistQueryResult } from '../../../core/common/cqers/query/queries/media/result/DoesMediaExistQueryResult';
+import { DoesMediaExistQueryHandler } from '../../../core/domain/media/handler/DoesMediaExistQueryHandler';
+import { Inject, Injectable } from '@nestjs/common';
+import { MediaDITokens } from '../../../core/domain/media/di/MediaDITokens';
+
+@Injectable()
+@QueryHandler(DoesMediaExistQuery)
+export class NestDoesMediaExistQueryHandler implements IQueryHandler {
+  
+  constructor(
+    @Inject(MediaDITokens.DoesMediaExistQueryHandler)
+    private readonly handleService: DoesMediaExistQueryHandler
+  ) {}
+
+  public async execute(query: DoesMediaExistQuery): Promise<DoesMediaExistQueryResult> {
+    return this.handleService.handle(query);
+  }
+  
+}

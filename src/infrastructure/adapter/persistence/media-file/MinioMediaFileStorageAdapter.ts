6e21f0d8f2a5d8e6f3d00dea44e559aac03328cd
pvarentsov <varentsovpavel@gmail.com>
2020-07-14 18:49:50 +0300

    Add MinioMediaFileStorageAdapter

---
+++ src/infrastructure/adapter/persistence/media-file/MinioMediaFileStorageAdapter.ts
@@ -0,0 +1,47 @@
+import { MediaFileStoragePort } from '../../../../core/domain/media/port/persistence/MediaFileStoragePort';
+import * as Minio from 'minio';
+import { BucketItemStat } from 'minio';
+import { Readable } from 'stream';
+import { MediaFileStorageOptions } from '../../../../core/common/persistence/MediaFileStorageOptions';
+import { FileMetadata } from '../../../../core/domain/media/value-object/FileMetadata';
+import { v4 } from 'uuid';
+
+/**
+ * TODO:
+ * 1. Add config
+ * 2. Remove hardcode
+ */
+
+export class MinioMediaFileStorageAdapter implements MediaFileStoragePort {
+  
+  private client: Minio.Client = new Minio.Client({
+    endPoint : 'localhost',
+    port     : 9000,
+    accessKey: 'accessKey',
+    secretKey: 'secretKey',
+    useSSL   : false
+  });
+  
+  public async upload(file: Buffer|Readable, options: MediaFileStorageOptions): Promise<FileMetadata> {
+    const bucket: string = 'images';
+    const key: string = `${v4()}.png`;
+    
+    await this.client.putObject(bucket, key, file, {
+      'Content-Type': 'image/png',
+      'Public': options.public,
+      'Type': options.type
+    });
+    
+    const fileStat: BucketItemStat = await this.client.statObject(bucket, key);
+    
+    const fileMetadata: FileMetadata = await FileMetadata.new({
+      relativePath: `${bucket}/${key}`,
+      size: fileStat.size,
+      mimetype: fileStat.metaData['Content-Type'],
+      ext: 'png'
+    });
+    
+    return fileMetadata;
+  }
+
+}

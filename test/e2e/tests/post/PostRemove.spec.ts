169f19b2ecde22c3490910b0b59795a56e1eec39
pvarentsov <varentsovpavel@gmail.com>
2020-09-20 01:18:48 +0300

    Add post e2e tests

--- test/e2e/tests/post/PostRemove.spec.ts
+++ test/e2e/tests/post/PostRemove.spec.ts
@@ -1,58 +1,69 @@
 import { Code } from '@core/common/code/Code';
 import { UserRole } from '@core/common/enums/UserEnums';
-import { MediaDITokens } from '@core/domain/media/di/MediaDITokens';
-import { Media } from '@core/domain/media/entity/Media';
-import { MediaRepositoryPort } from '@core/domain/media/port/persistence/MediaRepositoryPort';
+import { PostDITokens } from '@core/domain/post/di/PostDITokens';
+import { Post } from '@core/domain/post/entity/Post';
+import { PostRepositoryPort } from '@core/domain/post/port/persistence/PostRepositoryPort';
 import { User } from '@core/domain/user/entity/User';
-import { RemoveMediaAdapter } from '@infrastructure/adapter/usecase/media/RemoveMediaAdapter';
+import { RemovePostAdapter } from '@infrastructure/adapter/usecase/post/RemovePostAdapter';
 import { HttpStatus } from '@nestjs/common';
 import { TestServer } from '@test/.common/TestServer';
 import { AuthExpect } from '@test/e2e/expect/AuthExpect';
 import { ResponseExpect } from '@test/e2e/expect/ResponseExpect';
 import { AuthFixture } from '@test/e2e/fixture/AuthFixture';
-import { MediaFixture } from '@test/e2e/fixture/MediaFixture';
+import { PostFixture } from '@test/e2e/fixture/PostFixture';
 import { UserFixture } from '@test/e2e/fixture/UserFixture';
 import * as supertest from 'supertest';
 import { v4 } from 'uuid';
 
-describe('Media.Remove', () => {
+describe('Post.Remove', () => {
   
   let testServer: TestServer;
   
   let userFixture: UserFixture;
-  let mediaFixture: MediaFixture;
+  let postFixture: PostFixture;
   
-  let mediaRepository: MediaRepositoryPort;
+  let postRepository: PostRepositoryPort;
   
   beforeAll(async () => {
     testServer = await TestServer.new();
     
     userFixture = UserFixture.new(testServer.testingModule);
-    mediaFixture = MediaFixture.new(testServer.testingModule);
+    postFixture = PostFixture.new(testServer.testingModule);
   
-    mediaRepository = testServer.testingModule.get(MediaDITokens.MediaRepository);
+    postRepository = testServer.testingModule.get(PostDITokens.PostRepository);
     
     await testServer.serverApplication.init();
   });
   
-  describe('DELETE /medias/{mediaId}', () => {
+  describe('DELETE /posts/{postId}', () => {
     
-    test('When admin removes media, expect it removes media', async () => {
-      await expectItRemovesMedia(UserRole.ADMIN, testServer, mediaRepository, userFixture, mediaFixture);
+    test('When author removes post, expect it removes post', async () => {
+      await expectItRemovesPost(UserRole.AUTHOR, testServer, postRepository, userFixture, postFixture);
+    });
+  
+    test('When admin removes post, expect it returns "ACCESS_DENIED_ERROR" response', async () => {
+      const executor: User = await userFixture.insertUser({role: UserRole.ADMIN, email: `${v4()}@email.com`, password: v4()});
+      const auth: {accessToken: string} = await AuthFixture.loginUser({id: executor.getId()});
+    
+      const post: Post = await postFixture.insertPost({owner: executor});
+    
+      const response: supertest.Response = await supertest(testServer.serverApplication.getHttpServer())
+        .delete(`/posts/${post.getId()}`)
+        .set('x-api-token', auth.accessToken)
+        .expect(HttpStatus.OK);
+    
+      ResponseExpect.codeAndMessage(response.body, {code: Code.ACCESS_DENIED_ERROR.code, message: Code.ACCESS_DENIED_ERROR.message});
+      ResponseExpect.data({response: response.body}, null);
     });
     
-    test('When author removes media, expect it removes media', async () => {
-      await expectItRemovesMedia(UserRole.AUTHOR, testServer, mediaRepository, userFixture, mediaFixture);
-    });
-    
-    test('When guest removes media, expect it returns "ACCESS_DENIED_ERROR" response', async () => {
+    test('When guest removes post, expect it returns "ACCESS_DENIED_ERROR" response', async () => {
       const executor: User = await userFixture.insertUser({role: UserRole.GUEST, email: `${v4()}@email.com`, password: v4()});
       const auth: {accessToken: string} = await AuthFixture.loginUser({id: executor.getId()});
   
-      const media: Media = await mediaFixture.insertMedia();
+      const post: Post = await postFixture.insertPost({owner: executor});
       
       const response: supertest.Response = await supertest(testServer.serverApplication.getHttpServer())
-        .delete(`/medias/${media.getId()}`)
+        .delete(`/posts/${post.getId()}`)
         .set('x-api-token', auth.accessToken)
         .expect(HttpStatus.OK);
       
@@ -60,16 +71,15 @@ describe('Media.Remove', () => {
       ResponseExpect.data({response: response.body}, null);
     });
   
-    test('When user try to remove other people\'s media, expect it returns "ACCESS_DENIED_ERROR" response', async () => {
-      const ownerId: string = v4();
-      
+    test('When user try to remove other people\'s post, expect it returns "ACCESS_DENIED_ERROR" response', async () => {
       const executor: User = await userFixture.insertUser({role: UserRole.AUTHOR, email: `${v4()}@email.com`, password: v4()});
       const {accessToken} = await AuthFixture.loginUser({id: executor.getId()});
-    
-      const media: Media = await mediaFixture.insertMedia({ownerId});
+  
+      const owner: User = await userFixture.insertUser({role: UserRole.AUTHOR, email: `${v4()}@email.com`, password: v4()});
+      const post: Post = await postFixture.insertPost({owner});
     
       const response: supertest.Response = await supertest(testServer.serverApplication.getHttpServer())
-        .delete(`/medias/${media.getId()}`)
+        .delete(`/posts/${post.getId()}`)
         .set('x-api-token', accessToken)
         .expect(HttpStatus.OK);
     
@@ -78,25 +88,29 @@ describe('Media.Remove', () => {
     });
     
     test('When access token is not passed, expect it returns "UNAUTHORIZED_ERROR" response', async () => {
-      const media: Media = await mediaFixture.insertMedia();
-      await AuthExpect.unauthorizedError({method: 'delete', path: `/medias/${media.getId()}`}, testServer);
+      const executor: User = await userFixture.insertUser({role: UserRole.AUTHOR, email: `${v4()}@email.com`, password: v4()});
+      const post: Post = await postFixture.insertPost({owner: executor});
+      
+      await AuthExpect.unauthorizedError({method: 'delete', path: `/posts/${post.getId()}`}, testServer);
     });
     
     test('When access token is not valid, expect it returns "UNAUTHORIZED_ERROR" response', async () => {
-      const media: Media = await mediaFixture.insertMedia();
-      await AuthExpect.unauthorizedError({method: 'delete', path: `/medias/${media.getId()}`}, testServer, v4());
+      const executor: User = await userFixture.insertUser({role: UserRole.AUTHOR, email: `${v4()}@email.com`, password: v4()});
+      const post: Post = await postFixture.insertPost({owner: executor});
+      
+      await AuthExpect.unauthorizedError({method: 'delete', path: `/posts/${post.getId()}`}, testServer, v4());
     });
   
-    test('When user try to remove not existing media, expect it returns "ENTITY_NOT_FOUND_ERROR" response', async () => {
-      const executor: User = await userFixture.insertUser({role: UserRole.ADMIN, email: `${v4()}@email.com`, password: v4()});
+    test('When user try to remove not existing post, expect it returns "ENTITY_NOT_FOUND_ERROR" response', async () => {
+      const executor: User = await userFixture.insertUser({role: UserRole.AUTHOR, email: `${v4()}@email.com`, password: v4()});
       const {accessToken} = await AuthFixture.loginUser({id: executor.getId()});
       
       const response: supertest.Response = await supertest(testServer.serverApplication.getHttpServer())
-        .delete(`/medias/${v4()}`)
+        .delete(`/posts/${v4()}`)
         .set('x-api-token', accessToken)
         .expect(HttpStatus.OK);
     
-      ResponseExpect.codeAndMessage(response.body, {code: Code.ENTITY_NOT_FOUND_ERROR.code, message: 'Media not found.'});
+      ResponseExpect.codeAndMessage(response.body, {code: Code.ENTITY_NOT_FOUND_ERROR.code, message: 'Post not found.'});
       ResponseExpect.data({response: response.body}, null);
     });
     
@@ -105,12 +119,12 @@ describe('Media.Remove', () => {
       const auth: {accessToken: string} = await AuthFixture.loginUser({id: executor.getId()});
       
       const response: supertest.Response = await supertest(testServer.serverApplication.getHttpServer())
-        .delete('/medias/not-uuid')
+        .delete('/posts/not-uuid')
         .set('x-api-token', auth.accessToken)
         .expect(HttpStatus.OK);
       
-      expect(response.body.data.context).toBe(RemoveMediaAdapter.name);
-      expect(response.body.data.errors.map((error: Record<string, unknown>) => error.property)).toEqual(['mediaId']);
+      expect(response.body.data.context).toBe(RemovePostAdapter.name);
+      expect(response.body.data.errors.map((error: Record<string, unknown>) => error.property)).toEqual(['postId']);
       
       ResponseExpect.codeAndMessage(response.body, {code: Code.USE_CASE_PORT_VALIDATION_ERROR.code, message: Code.USE_CASE_PORT_VALIDATION_ERROR.message});
     });
@@ -125,39 +139,39 @@ describe('Media.Remove', () => {
   
 });
 
-async function expectItRemovesMedia(
+async function expectItRemovesPost(
   executorRole: UserRole,
   testServer: TestServer,
-  mediaRepository: MediaRepositoryPort,
+  postRepository: PostRepositoryPort,
   userFixture: UserFixture,
-  mediaFixture: MediaFixture
+  postFixture: PostFixture
 
 ): Promise<void> {
   
   const executor: User = await userFixture.insertUser({role: executorRole, email: `${v4()}@email.com`, password: v4()});
   const auth: {accessToken: string} = await AuthFixture.loginUser({id: executor.getId()});
   
-  const media: Media = await mediaFixture.insertMedia({ownerId: executor.getId()});
+  const post: Post = await postFixture.insertPost({owner: executor});
   
   const removeResponse: supertest.Response = await supertest(testServer.serverApplication.getHttpServer())
-    .delete(`/medias/${media.getId()}`)
+    .delete(`/posts/${post.getId()}`)
     .set('x-api-token', auth.accessToken)
     .expect(HttpStatus.OK);
   
   const getResponse: supertest.Response = await supertest(testServer.serverApplication.getHttpServer())
-    .get(`/medias/${v4()}`)
+    .get(`/posts/${v4()}`)
     .set('x-api-token', auth.accessToken)
     .expect(HttpStatus.OK);
   
   const getListResponse: supertest.Response = await supertest(testServer.serverApplication.getHttpServer())
-    .get('/medias')
+    .get('/posts')
     .set('x-api-token', auth.accessToken)
     .expect(HttpStatus.OK);
   
   ResponseExpect.codeAndMessage(removeResponse.body, {code: Code.SUCCESS.code, message: Code.SUCCESS.message});
   ResponseExpect.data({response: removeResponse.body}, null);
   
-  ResponseExpect.codeAndMessage(getResponse.body, {code: Code.ENTITY_NOT_FOUND_ERROR.code, message: 'Media not found.'});
+  ResponseExpect.codeAndMessage(getResponse.body, {code: Code.ENTITY_NOT_FOUND_ERROR.code, message: 'Post not found.'});
   ResponseExpect.data({response: getResponse.body}, null);
   
   ResponseExpect.codeAndMessage(getListResponse.body, {code: Code.SUCCESS.code, message: Code.SUCCESS.message});

763e0fefc2f6c243d56e05546f31d0f4c98aaad1
pvarentsov <varentsovpavel@gmail.com>
2020-09-20 23:56:17 +0300

    Pass port in transactional use-cases

--- src/core/common/usecase/TransactionalUseCase.ts
+++ src/core/common/usecase/TransactionalUseCase.ts
@@ -1,7 +1,7 @@
 import { UseCase } from '@core/common/usecase/UseCase';
 
 export interface TransactionalUseCase<TUseCasePort, TUseCaseResult> extends UseCase<TUseCasePort, TUseCaseResult> {
-  onCommit(result?: TUseCaseResult): Promise<void>;
-  onRollback(error?: Error): Promise<void>
+  onCommit(result: TUseCaseResult, port: TUseCasePort): Promise<void>;
+  onRollback(error: Error, port: TUseCasePort): Promise<void>
 }
 

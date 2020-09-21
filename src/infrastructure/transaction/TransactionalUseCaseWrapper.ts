56a266a8defb8c6079c813a235427f494d704d47
pvarentsov <varentsovpavel@gmail.com>
2020-09-22 00:25:44 +0300

    Fix TransactionalUseCaseWrapper generics

--- src/infrastructure/transaction/TransactionalUseCaseWrapper.ts
+++ src/infrastructure/transaction/TransactionalUseCaseWrapper.ts
@@ -2,17 +2,17 @@ import { TransactionalUseCase } from '@core/common/usecase/TransactionalUseCase'
 import { UseCase } from '@core/common/usecase/UseCase';
 import { runOnTransactionCommit, runOnTransactionRollback, Transactional } from 'typeorm-transactional-cls-hooked';
 
-export class TransactionalUseCaseWrapper implements UseCase<unknown, unknown> {
+export class TransactionalUseCaseWrapper<TUseCasePort, TUseCaseResult> implements UseCase<TUseCasePort, TUseCaseResult> {
   
   constructor(
-    private readonly useCase: TransactionalUseCase<unknown, unknown>,
+    private readonly useCase: TransactionalUseCase<TUseCasePort, TUseCaseResult>,
   ) {}
   
   @Transactional()
-  public async execute(port?: unknown): Promise<unknown> {
+  public async execute(port: TUseCasePort): Promise<TUseCaseResult> {
     runOnTransactionRollback(async (error: Error) => this.useCase.onRollback?.(error, port));
     
-    const result: unknown = await this.useCase.execute(port);
+    const result: TUseCaseResult = await this.useCase.execute(port);
     runOnTransactionCommit(async () => this.useCase.onCommit?.(result, port));
     
     return result;
